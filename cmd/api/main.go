88a9b0c1c41cf097fefb62d02d84d722a7959198
anoulack007 <anoulack1900@gmail.com>
2026-03-05 14:04:12 +0700

    add struct in domain

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/anoulack007/core-pos/config"
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -13,11 +14,32 @@ func main() {
 	cfg := config.LoadConfig()
 
 	// 2. Connect to DB
-	db, err := gorm.Open(postgres.Open(cfg.DSN()),&gorm.Config{})
+	db, err := gorm.Open(postgres.Open(cfg.DSN()), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
 	}
 
 	log.Println("Database connected successfully!")
+
+	r := gin.Default()
+
+	api := r.Group("/api/v1")
+	{
+		r.GET("/health", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"message": "OK",
+			})
+		})
+
+		// Store-scoped routes
+		store := api.Group("/stores/:storeId")
+		{
+		}
+	}
+
+	log.Printf("Server starting on port %s", cfg.AppPort)
+	if err := r.Run(":" + cfg.AppPort); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 
 }
